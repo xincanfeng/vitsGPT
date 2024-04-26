c222db9c052670f867227509b2609704f10dcf6a
xincan <cecilia.fung@foxmail.com>
2024-04-26 22:03:36 +0000

    u

--- datasets/download_LibriTTS.sh
+++
@@ -1,9 +0,0 @@
-# download_LibriTTS
-# dev-clean.tar.gz [1.2G] 
-wget https://openslr.magicdatatech.com/resources/60/dev-clean.tar.gz
-
-# test-clean.tar.gz [1.2G]  
-wget https://openslr.magicdatatech.com/resources/60/test-clean.tar.gz
-
-# train-clean-100.tar.gz [7.7G]
-wget https://openslr.magicdatatech.com/resources/60/train-clean-100.tar.gz
