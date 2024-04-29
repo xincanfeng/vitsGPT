eed82acdab78804cc87093c443f1d3c70bad18d2
xincan <cecilia.fung@foxmail.com>
2024-04-29 12:00:32 +0000

    update

--- vits/sem_vits/sem_train.sh
+++ vits/sem_vits/sem_train.sh
@@ -1,9 +1,9 @@
 # LJSpeech
 # batchsize 64
 CUDA_VISIBLE_DEVICES=0 python sem_train.py -c configs/onehour_ljs_sem_mat_text.json -m onehour_ljs_sem_mat_text
-CUDA_VISIBLE_DEVICES=0 python sem_train.py -c configs/onehour_ljs_sem_mat_phone.json -m onehour_ljs_sem_mat_phone
-CUDA_VISIBLE_DEVICES=0 python sem_train.py -c configs/onehour_ljs_bert_text.json -m onehour_ljs_sem_mat_bert_text
-CUDA_VISIBLE_DEVICES=0 python sem_train.py -c configs/onehour_ljs_bert_phone.json -m onehour_ljs_sem_mat_bert_phone
+CUDA_VISIBLE_DEVICES=1 python sem_train.py -c configs/onehour_ljs_sem_mat_phone.json -m onehour_ljs_sem_mat_phone
+# CUDA_VISIBLE_DEVICES=0 python sem_train.py -c configs/onehour_ljs_bert_text.json -m onehour_ljs_sem_mat_bert_text
+# CUDA_VISIBLE_DEVICES=0 python sem_train.py -c configs/onehour_ljs_bert_phone.json -m onehour_ljs_sem_mat_bert_phone
 
 CUDA_VISIBLE_DEVICES=0 python sem_train.py -c configs/ljs_sem_mat_text.json -m ljs_sem_mat_text
 CUDA_VISIBLE_DEVICES=0 python sem_train.py -c configs/ljs_sem_mat_phone.json -m ljs_sem_mat_phone
@@ -13,9 +13,9 @@ CUDA_VISIBLE_DEVICES=0 python sem_train.py -c configs/ljs_bert_phone.json -m ljs
 
 # EmoV_DB
 # batchsize 16
-CUDA_VISIBLE_DEVICES=0 python sem_train.py -c configs/emovdb_sem_mat_text16.json -m emovdb_sem_mat_text_pretrained16
-CUDA_VISIBLE_DEVICES=0 python sem_train.py -c configs/emovdb_sem_mat_phone16.json -m emovdb_sem_mat_phone_pretrained16
-CUDA_VISIBLE_DEVICES=0 python sem_train.py -c configs/emovdb_bert_text16.json -m emovdb_sem_mat_bert_text_pretrained16
-CUDA_VISIBLE_DEVICES=0 python sem_train.py -c configs/emovdb_bert_phone16.json -m emovdb_sem_mat_bert_phone_pretrained16
+# CUDA_VISIBLE_DEVICES=0 python sem_train.py -c configs/emovdb_sem_mat_text16.json -m emovdb_sem_mat_text_pretrained16
+# CUDA_VISIBLE_DEVICES=0 python sem_train.py -c configs/emovdb_sem_mat_phone16.json -m emovdb_sem_mat_phone_pretrained16
+# CUDA_VISIBLE_DEVICES=0 python sem_train.py -c configs/emovdb_bert_text16.json -m emovdb_sem_mat_bert_text_pretrained16
+# CUDA_VISIBLE_DEVICES=0 python sem_train.py -c configs/emovdb_bert_phone16.json -m emovdb_sem_mat_bert_phone_pretrained16
 
 
