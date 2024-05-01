e3beed231c20e620e43801756b88ef6e7021b3c9
xincan <cecilia.fung@foxmail.com>
2024-05-01 07:48:38 +0000

    update url

--- vits/sem_vits/sem_train.sh
+++ vits/sem_vits/sem_train.sh
@@ -2,8 +2,8 @@
 # batchsize 64
 CUDA_VISIBLE_DEVICES=0 python sem_train.py -c configs/onehour_ljs_sem_mat_text.json -m onehour_ljs_sem_mat_text
 CUDA_VISIBLE_DEVICES=1 python sem_train.py -c configs/onehour_ljs_sem_mat_phone.json -m onehour_ljs_sem_mat_phone
-# CUDA_VISIBLE_DEVICES=0 python sem_train.py -c configs/onehour_ljs_bert_text.json -m onehour_ljs_sem_mat_bert_text
-# CUDA_VISIBLE_DEVICES=0 python sem_train.py -c configs/onehour_ljs_bert_phone.json -m onehour_ljs_sem_mat_bert_phone
+CUDA_VISIBLE_DEVICES=0 python sem_train.py -c configs/onehour_ljs_bert_text.json -m onehour_ljs_sem_mat_bert_text
+CUDA_VISIBLE_DEVICES=0 python sem_train.py -c configs/onehour_ljs_bert_phone.json -m onehour_ljs_sem_mat_bert_phone
 
 CUDA_VISIBLE_DEVICES=0 python sem_train.py -c configs/ljs_sem_mat_text.json -m ljs_sem_mat_text
 CUDA_VISIBLE_DEVICES=0 python sem_train.py -c configs/ljs_sem_mat_phone.json -m ljs_sem_mat_phone
